57533fd58623617bf327afa2cb77a1432c151056
Michał Trąbski <michaltrabskiport@gmail.com>
2021-06-07 09:24:35 +0200

    initial commit

---
+++ src/components/Player.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
+
+export default function Player() {
+  return (
+    <video controls src="file:///C:/Users/mtrabski/Desktop/video1.mp4"></video>
+  );
+}
